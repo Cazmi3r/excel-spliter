595ff0ddf3030b8dc97c87484ad60bd446016ae4
Andrew Paulikas <PaulikasA@Gmail.com>
2023-05-26 11:33:21 -0400

    moved script into function

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import pandas as pd
+from pathlib import Path
 
-input = "input.xlsx"
 
-df_dic = pd.read_excel(input, sheet_name=None)
+input = Path("./input.xlsx")
+output = Path(".")
 
-for sheet in df_dic:
-    df_dic[sheet].to_excel(f"{sheet}.xlsx", index=False)
+def excel_split(input_path, output_path):
+    df_dic = pd.read_excel(input_path, sheet_name=None)
+    for sheet in df_dic:
+        file_name = output_path / f"{sheet}.xlsx"
+        df_dic[sheet].to_excel(file_name, index=False)
+
+excel_split(input,output)
