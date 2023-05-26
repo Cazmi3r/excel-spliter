a03779d2059a9f7c8f57b49783cdadc7d2ad4690
Andrew Paulikas <PaulikasA@Gmail.com>
2023-05-26 11:01:04 -0400

    no longer cuts off bottom row

--- main.py
+++ main.py
@@ -6,8 +6,5 @@ df_dic = pd.read_excel(input, sheet_name=None)
 
 for sheet in df_dic:
     df = df_dic[sheet]
-    
-    # removes the last row. pandas was adding a sum as the last row
-    df.drop(df.tail(1).index,inplace=True)
     df.to_excel(f"{sheet}.xlsx", index=False)
     
