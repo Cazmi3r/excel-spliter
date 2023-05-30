f3b5328f223513d1857ca4fb2842f555de1722f9
Cazmi3r <PaulikasA@gmail.com>
2023-05-30 11:43:58 -0400

    update UI

--- app.py
+++ app.py
@@ -10,15 +10,16 @@ class App(customtkinter.CTk):
         self.output_path = ""
 
         self.title("Excel Splitter")
-        self.geometry("400x180")
+        self.geometry("400x90")
         self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
         
         self.file_button = customtkinter.CTkButton(self, text="Select a File", command=self.file_button_callback)
-        self.file_button.grid(row=0, column=0, padx=3, pady=3, sticky="ew")
+        self.file_button.grid(row=0, column=0, padx=10, pady=10, sticky="ews")
 
         self.submit_button = customtkinter.CTkButton(self, text="Submit", command=self.submit_button_callback)
-        self.submit_button.grid(row=1, column=0, padx=3, pady=3, sticky="ew")
+        self.submit_button.grid(row=0, column=1, padx=10, pady=10, sticky="ews")
         
     def set_output_path(self):
         self.output_path = self.file_path.parent
