7a1e68920f519196c8a37d74b54a85cd84cde491
Cazmi3r <PaulikasA@gmail.com>
2023-05-30 15:08:36 -0400

    add error handling

--- app.py
+++ app.py
@@ -23,17 +23,36 @@ class App(customtkinter.CTk):
 
         self.submit_button = customtkinter.CTkButton(self, text="Submit", command=self.submit_button_callback)
         self.submit_button.grid(row=0, column=1, padx=10, pady=10, sticky="ews")
+    
+    def validate_file_path(self):
+        
+        if self.file_path == "":
+            self.label.configure(text="ERROR! No file selected!")
+            return False
+            
+        file_suffix = self.file_path.suffix
+        if ".xlsx" not in file_suffix:
+            self.label.configure(text="ERROR! Only .xls or .xlsx files work!")
+            return False
+        
+        return True
+            
         
     def set_output_path(self):
         self.output_path = self.file_path.parent
 
     def submit_button_callback(self):
-        excel_split(self.file_path, self.output_path)
+        if self.validate_file_path():    
+            excel_split(self.file_path, self.output_path)
+            self.label.configure(text="File has been split!")
+            self.file_path = ""
+            self.output_path = ""
         
     def file_button_callback(self):
         f = customtkinter.filedialog.askopenfile(parent=self,mode='rb',title='Choose a file')
         self.file_path = Path(f.name)
         self.set_output_path()
+        self.label.configure(text=f"Selected File: {self.file_path.name}")
         
 app = App()
 app.mainloop()
