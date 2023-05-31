2b29a6d9b601ae7aad726cb6a69bc879cdb3f9f8
Andrew Paulikas <PaulikasA@Gmail.com>
2023-05-31 16:10:50 -0400

    add sheet number to file name

--- excelSpliter.py
+++ excelSpliter.py
@@ -1,7 +1,8 @@
 import pandas as pd
-# TODO add '-x' to the end of all output files where x is the sheet number in the input
 def excel_split(input_path, output_path):
     df_dic = pd.read_excel(input_path, sheet_name=None)
+    sheet_num = 1
     for sheet in df_dic:
-        file_name = output_path / f"{sheet}.xlsx"
+        file_name = output_path / f"{sheet}-{sheet_num}.xlsx"
         df_dic[sheet].to_excel(file_name, index=False)
+        sheet_num = sheet_num + 1
