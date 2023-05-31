428f4769396d73091338a362e6c527d1f91556fe
Andrew Paulikas <PaulikasA@Gmail.com>
2023-05-31 12:35:12 -0400

    added todo

--- excelSpliter.py
+++ excelSpliter.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+# TODO add '-x' to the end of all output files where x is the sheet number in the input
 def excel_split(input_path, output_path):
     df_dic = pd.read_excel(input_path, sheet_name=None)
     for sheet in df_dic:
